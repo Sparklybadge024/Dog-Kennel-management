efd6d0122309787fd17a90ada3a9f4f18966262b
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-10-15 21:45:32 +0530

    Initial commit

---
+++ DogKennel.java
@@ -0,0 +1,11 @@
+class Dog{
+    String name;
+    byte age;
+    boolean vaccinated;
+}
+
+public class DogKennel{
+    public static void main(String[] args){
+      Dog dogKennelStep1=new Dog();   
+    }
+}
