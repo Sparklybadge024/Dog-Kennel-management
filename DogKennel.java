53a80a0e73c1f17372c2a24384f0b740b6702001
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-10-16 18:49:17 +0530

    Dog class and Kennel class with the main has been constructed

--- DogKennel.java
+++ DogKennel.java
@@ -5,7 +5,11 @@ class Dog{
 }
 
 public class DogKennel{
-    public static void main(String[] args){
-      Dog dogKennelStep1=new Dog();   
+public static void main(String[] args){
+    Dog dogKennelStep1=new Dog();  
+    dogKennelStep1.name="Bruno";
+    dogKennelStep1.age=8;
+    dogKennelStep1.vaccinated=true;
+    System.out.println("Dog name is "+dogKennelStep1.name+" he is "+dogKennelStep1.age+" years old and his vaccination status is: "+dogKennelStep1.vaccinated);
     }
 }
